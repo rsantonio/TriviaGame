f7d59f3a0086508742aafa2259c71683f5054501
Ruth San Antonio <RuthS.Antonio@gmail.com>
2017-09-02 21:36:20 -0700

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,110 +1,120 @@
-$(document).ready(function() {
-	$(".reset").click(function(){
-		location.reload(true);
-	});
+var rightAnswer = 0;
+var wrongAnswer = 0;
+var noAnswer = 0;
+var allocatedTime = 30;
+var timer; 
+//question array
 
-	$(".start").click(gameQuiz);
-
-
-	function gameQuiz() {
-		var userScore =[];
-		var questions = [
+var arryQuestions = [
 	{
+		title: "question1",
 		question: "Who played Legolas in the LOTR movies?",
 		choices: ["Orlando Bloom", "Brad Pitt", "Gal Gadot", "Prince"],
-		correctChoice: 0
+		correctChoice: "a"
 	}, 
 	{
+		title: "question2",
 		question: "Who saved all of Middle Earth?",
-		choices: ["Sam", "Gandalf", "Tolkien","Galadriel"],
-		correctChoice: 0
+		choices: ["Sam", "Gandalf", "Tolkien", "Galadriel"],
+		correctChoice: "Sam"
 	}, 
 	{
+		title: "question3",
 		question: "Where must the hobbits go to destroy the one ring?",
-		choices: ["Rivendale", "The Shire","Mt. Doom","Rohan"],
-		correctChoice: 2
+		choices: ["Rivendale", "The Shire", "Mt. Doom", "Rohan"],
+		correctChoice: "Mt. Doom"
 	}, 
 	{
+		title: "question4",
 		question: "Who betrayed Gandalf and locked him in a tower?",
-		choices: ["Elrond","Saruman","Treebeard","Gollum"],
-		correctChoice: 1	
+		choices: ["Elrond", "Saruman", "Treebeard", "Gollum"],
+		correctChoice: "Saruman"	
 	}, 
 	{
+		title: "question5",
 		question: "Who had the ring before Frodo?",
-		choices: ["Gandalf","Legolas","Arwen", "Bildo"],
-		correctChoice: 2
+		choices: ["Gandalf", "Legolas", "Arwen", "Bildo"],
+		correctChoice: "Bildo"
 	}, 
 	{
+		title: "question6",
 		question: "How does one go into Mordor?",
-		choices: ["you climb", "haha, you're funny,one does not simply walk into Mordor","use the Nazgul","too scared to go"],
-		correctChoice: 1
+		choices: ["you climb", "haha, you're funny,one does not simply walk into Mordor", "use the Nazgul", "too scared to go"],
+		correctChoice: "haha, you're funny,one does not simply walk into Mordor"
+	}];
+
+//Display Timer
+
+function displayTimer() {
+	$(".time-remaining").html("Time Remaining<br>" + --sec + "<br>seconds");
+}
+
+function timeOver() {
+	alert("Time's up. Nice Try.");
+	checkResults();
+}
+
+function displayTrivia() {
+
+	setInterval(displayTimer, 1000);
+
+	timer = setTimeout(timeOver, 1000 * parseInt(sec));
+
+	var trivia = "";
+
+	for (var i = 0; i < arryQuestions.length; i++) {
+
+		var possibleAnswers = "";
+
+		for (var p = 0; p < arryQuestions[i].choices.length; p++) {
+			possibleAnswers += "<br><input name=" + arryQuestions[i].title + " type='radio' value=" + arryQuestions[i].choices[p] + ">&emsp;" + arryQuestions[i].choices[p];
+		}
+
+		trivia += "<span class= 'question'>" + arryQuestions[i].question + "</span>" + possibleAnswers + "<hr>";
 	}
-	];
+
+	$(".quiz-layout").append(trivia);
+	$(".form").show();
+	$(".start").hide();
+}
+
+function checkResults() {
+	var valid = "";
+	var answer = "";
+
+	for (var i = 0; i < arryQuestions.length; i++) {
+		valid = $("input[name='" + arryQuestions[i].title + "']:checked").attr("value");
+		answer = arryQuestions[i].correctChoice;
+
+		if(valid === answer) {
+			rightAnswer++;
+		} else if (valid === undefined) {
+			noAnswer++;
+		} else {
+			wrongAnswer++;
+		}
+	}
+
+	displayResults(rightAnswer,wrongAnswer,noAnswer);
 
 }
 
-	var questionCounter = questions.length;
+function displayResults(rightAnswer,wrongAnswer,noAnswer) {
+	var finishedTrivia = arryQuestions.length;
+	var message = "Your score: " + finishedTrivia + "<hr>Answered right: " + rightAnswer + "<br>Answered wrong: " + wrongAnswer + "<br>Unanswered: " + noAnswer;
+	$(".score").html(message);
+	$(".results").show();
+	$(".form").hide();
+}
 
-	function questionLayout(questions) {
-		for (var i = 0; i < questions.length; i++)
-		{
-			$(".start").hide();
-			$(".quizGame").append("<form id="' + i + '" class="center-text"><p>Question ' + (i + 1) + ' of ' + questions.length + '</p><h3 class="question">' + questions[i].question + '</h3>'+ radioButtons(questions[i].s, i) + '<button type="submit" class="next">NEXT &#8594;</button></p></form>');
-		}
-	}
+function submitForm() {
+	clearTimeout(timer);
+	checkResults();
+}
 
-	function radioButton(ary, qNum) {
-		var answers = [];
-		for (i = 0; i < ary.length; i++) {
-			answers.push('<label><input type="radio" name="' + qNum + '" value="' + ary[i] + '">' + ary[i] + '</label>')
-		}
-		return answers.join(" ");
-	}
+$(document).ready(function(){
 
-	function sumScore(questions) {
-         return scoreAry.reduce(function (previousValue, currentValue, index, array) {
-         return previousValue + currentValue;
-         });
-     }
+	$(".start").click(displayTrivia);
 
-            function checkAnswer(answer, qNum, questions) {
-            if (answer == questions[qNum].choices) {
-                questions[qNum].correct = 1;
-                userScore.push(questions[qNum].correctChoice);
-            } else {
-                userScore.push(questions[qNum].correctChoice);
-            }
-        }
-        
-        createQuestion(questions);
-        
-        $(".next").click(function (event) {
-            event.preventDefault(); //This stops the form from submitting
-            var qNum = $(this).closest("form").attr("id"); //This gives us the question number
-            var userInput = $('input[name=' + qNum + ']:radio:checked').val(); //This grabs the user's selected answer
-            if (counter > 1) {
-                checkAnswer(userInput, qNum, questions);
-                $("#" + qNum).hide();
-                $("#" + qNum).next().show();
-                counter--;
-            } else if (counter == 1) {
-                checkAnswer(userInput, qNum, questions);
-                $("#questions").find("form").remove();
-                $("#questions").append('<h3 class="result"></h3>');
-                $(".result").text('You answered ' + sumScore(questions) + ' questions correctly out of 6.');
-                   for (j = 0; j < userScore.length; j++) {
-                        if (userScore[j] === 0) {
-                            console.log(questions[j].question, questions[j].choices);
-                            $("#questions").append('<p class="missed-' + j + '">You missed: ' + questions[j].question + ' ' + questions[j].choices + '</p>');      
-                        }
-                    }
-            } else {
-                return false;
-            }
-        });
-    }
+	$(".submit").click(submitForm);
 });
-
-
-
-
